08dd060b5d8c033802e8d1ca639322f6e3f77d7f
michalbork <michalbork3@gmail.com>
2023-03-17 15:52:52 +0100

    squash begin

--- src/Console/UpdateDataCommand.php
+++ src/Console/UpdateDataCommand.php
@@ -2,10 +2,15 @@
 
 namespace App\Console;
 
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'app:update-data',
+    description: 'Update currency table',
+)]
 class UpdateDataCommand  extends Command
 {
 
